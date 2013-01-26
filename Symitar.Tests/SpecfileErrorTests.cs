cc7e2914d567e482e593cf3a3c5f3dc0dd017332
Jason Deering <deeringj2@gmail.com>
2013-01-25 20:04:01 -0500

    - Changing folder structure - Adding test project - Created first test

---
+++ Symitar.Tests/SpecfileErrorTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using FluentAssertions;
+
+namespace Symitar.Tests
+{
+    [TestFixture]
+    public class SpecfileErrorTests
+    {
+        [Test]
+        public void SpecfileError_None_DoesntFailCheck()
+        {
+            SpecfileError error = SpecfileError.None();
+            error.FailedCheck.Should().BeFalse();
+        }
+    }
+}
