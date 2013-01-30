7a341a8a3f8a82093a49ad8e18fafbd2ca8f4ad3
Jason Deering <deeringj2@gmail.com>
2013-01-30 13:33:50 -0500

    5 passing tests for ReportInfo

---
+++ Symitar.Tests/ReportInfoTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Symitar.Tests
+{
+    [TestFixture]
+    public class ReportInfoTests
+    {
+        [Test]
+        public void Constructor_ZeroSequence_ThrowsOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReportInfo(0, ""));
+        }
+
+        [Test]
+        public void Constructor_NegativeSequence_ThrowsOutOfRange()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new ReportInfo(-1, ""));
+        }
+
+        [Test]
+        public void Constructor_NullTitle_ThrowsArgumentNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => new ReportInfo(1, null));
+        }
+
+        [Test]
+        public void Constructor_ValidParameters_SetsCorrectSequence()
+        {
+            ReportInfo reportInfo = new ReportInfo(100, "DQ.REPORT");
+
+            reportInfo.Sequence.Should().Be(100);
+        }
+
+        [Test]
+        public void Constructor_ValidParameters_SetsCorrectTitle()
+        {
+            ReportInfo reportInfo = new ReportInfo(100, "DQ.REPORT");
+
+            reportInfo.Title.Should().Be("DQ.REPORT");
+        }
+    }
+}
--- Symitar/ReportInfo.cs
+++ Symitar/ReportInfo.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace Symitar
 {
     public struct ReportInfo
@@ -7,6 +9,9 @@ namespace Symitar
 
         public ReportInfo(int sequence, string title)
         {
+            if(sequence <= 0) throw new ArgumentOutOfRangeException("sequence", "Invalid report sequence number");
+            if(title == null) throw new ArgumentNullException("title");
+
             Sequence = sequence;
             Title = title;
         }
