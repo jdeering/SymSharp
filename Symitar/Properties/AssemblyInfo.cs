933ea58508a32a707e4ae6abb6058ba28e4c6f94
jasondeering <deeringj2@gmail.com>
2013-02-14 08:14:22 -0500

    Changing keys

--- Symitar/Properties/AssemblyInfo.cs
+++ Symitar/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyVersion("1.2.*")]
 
 // Exposing internals to the test framework
 [assembly: InternalsVisibleTo("Symitar.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ef2b2169dab3fbc5da1088266a3850650207aff73236c61d936528f310894ce2b5b2ef175b2dae3ad5e0dab6b1f4ef67392e4ee810e6813c1780b298af766bc907ac42ffccda21b15cd92fcce5c7d6bfd51a140669eaa4f171470bf1dbd039ff7f14fc151bd614cc468999ede691d1d8d56932ab2f7c8300ffbf18e3adb186ba")]
