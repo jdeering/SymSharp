0c1c7750fda244ae79835ccb948a98d15f2585dd
Jason Deering <jason@deering.me>
2015-01-02 14:36:18 -0600

    Set config for Release builds on AppVeyor

--- test.bat
+++ test.bat
@@ -1,1 +1,1 @@
-packages\NUnit.Runners.2.6.4\tools\nunit-console-x86 /noshadow .\Symitar.Tests\bin\Debug\Symitar.Tests.dll
+packages\NUnit.Runners.2.6.4\tools\nunit-console-x86 /noshadow .\Symitar.Tests\bin\Release\Symitar.Tests.dll
